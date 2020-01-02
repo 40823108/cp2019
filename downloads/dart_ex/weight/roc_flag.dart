97de52697fb2ab9864c296ca2ce8943640fedecb
40823108 <40823108@gm.nfu.edu.tw>
2020-01-02 10:42:11 +0800

    重量轉換

---
+++ downloads/dart_ex/weight/roc_flag.dart
@@ -0,0 +1,29 @@
+import "dart:html";
+kgtolb(num kg){
+return kg*2.204;
+}
+lbtokg(num lb){
+return lb*0.454;
+}
+main() {
+    InputElement tempInput = querySelector("#temp");
+    querySelector("#submit").onClick.listen((e) => convert(tempInput.value));
+}
+
+convert(String data){
+  int len;
+  var type;
+  var number;
+  LabelElement output = querySelector("#output");
+  len = data.length;
+  type = data[len-2];
+  number = data.substring(0, len-2);
+  number = int.parse(number);
+  if (type == "k" || type == "k"){
+     output.innerHtml = " $number 公斤 =  ${kgtolb(number).toStringAsFixed(2)} 磅";
+  } else if ((type == "l" || type == "l")){
+     output.innerHtml = " $number 磅 = ${lbtokg(number).toStringAsFixed(2)} 公斤";
+  } else {
+     output.innerHtml = "請輸入數字加上 kg 或 lb!";
+  }
+} // convert
